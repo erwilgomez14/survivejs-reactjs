1e7440bb38a3e9dda8418e853846546beaa3fc6d
erwilgomez14 <erwildiaz@gmail.com>
2023-07-27 14:28:42 -0400

    New project based on react-boilerplate (v2.5.6)

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Notes from './components/Notes';
+
+if(process.env.NODE_ENV !== 'production') {
+  React.Perf = require('react-addons-perf');
+}
+
+ReactDOM.render(
+  // <div>Hello world hollaaaaaaaaaaa</div>,
+  <Notes />,
+  document.getElementById('app')
+);
